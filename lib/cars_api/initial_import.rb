0be996fbe86a77b91dbc29697e2d42af84f81b61
Oleksii Fedorov <waterlink000@gmail.com>
2016-03-04 11:43:55 +0100

    InitialImport: define value objects and protocols

--- lib/cars_api/initial_import.rb
+++ lib/cars_api/initial_import.rb
@@ -2,5 +2,38 @@ require "cars_api"
 
 module CarsApi
   module InitialImport
+    # Request represents InitialImport use case input data
+    Request = Struct.new(:data_source)
+
+    # Response represents InitialImport use case output data
+    Response = Struct.new(:ok, :error)
+
+    # Describes protocol for DataSource required by InitialImport
+    class DataSourceProtocol
+      # @!group Required Methods
+
+      # @!method each
+      #   @yield [raw_car] Block to yield to with each car
+      #     data
+      #   @yieldparam raw_car [Hash] Car data in the form of hash
+
+      # @!endgroup
+    end
+
+    # Describes protocol for CarStore required by InitialImport
+    class CarStoreProtocol
+      # @!group Required Methods
+
+      # @!method save(car)
+      #   Saves a car to a car store
+      #   @param car [Car] Car to save
+      #   @return [String] Error message, nil if succeeded
+
+      # @!method clear
+      #   Clears data from the car store. Use with caution
+      #   @return [String] Error message, nil if succeeded
+
+      # @!endgroup
+    end
   end
 end
--- lib/cars_api/initial_import/interactor.rb
+++ lib/cars_api/initial_import/interactor.rb
@@ -2,6 +2,8 @@ require "cars_api/initial_import"
 
 module CarsApi
   module InitialImport
+    # job: InitialImport::Interactor represents an
+    # 'InitialImport' usecase business logic.
     class Interactor
     end
   end
