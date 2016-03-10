35016f483b04e3cd54e8ef7b034691f51a9bf571
Oleksii Fedorov <waterlink000@gmail.com>
2016-03-10 10:42:17 +0100

    Use 'bundle exec' in smoke tests to make sure command is available

--- spec/system/cars_endpoint_spec.rb
+++ spec/system/cars_endpoint_spec.rb
@@ -2,7 +2,7 @@ require "json"
 
 RSpec.describe "System tests" do
   describe "/cars API endpoint" do
-    let!(:pipe) { open("|cars_api server --server=webrick") }
+    let!(:pipe) { open("|bundle exec cars_api server --server=webrick") }
 
     after do
       Process.kill("INT", pipe.pid)
@@ -10,7 +10,7 @@ RSpec.describe "System tests" do
 
     context "when car store is empty" do
       it "is empty" do
-        `cars_api import ./spec/fixtures/empty.json`
+        `bundle exec cars_api import ./spec/fixtures/empty.json`
         markers = fetch_data
         expect(markers["cars"].count).to eq(0)
       end
@@ -18,7 +18,7 @@ RSpec.describe "System tests" do
 
     context "when car store has something" do
       it "returns some car markers" do
-        `cars_api import ./spec/fixtures/data.json`
+        `bundle exec cars_api import ./spec/fixtures/data.json`
         markers = fetch_data
         expect(markers["cars"].count).to be > 0
       end
